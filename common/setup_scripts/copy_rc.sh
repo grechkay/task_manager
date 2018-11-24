6d82e0682e55c05c38d5ace6f718c7a14bc555ca
yg <yg@test.com>
2018-11-23 20:09:11 -0800

    Modified tasks; adding more scripts; adding first note.

---
+++ common/setup_scripts/copy_rc.sh
@@ -0,0 +1,4 @@
+cp ~/core/common/rc_files/.bashrc ~
+cp ~/core/common/rc_files/.taskrc ~
+cp ~/core/common/rc_files/.vimrc ~
+
--- common/setup_scripts/initial_setup.sh
+++ common/setup_scripts/initial_setup.sh
@@ -1,7 +1,11 @@
-#bash Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
 
-#conda install nb_conda_kernels 
+conda install nb_conda_kernels 
 conda install -c conda-forge jupyterlab
+conda install matplotlib
+pip install taskw
+
 conda env create -f basenv.yml
+bash copy_rc.sh
 
 # Add copy scripts to copy .rc files
