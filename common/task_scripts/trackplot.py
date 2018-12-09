0923b83f24f945fc544b032c10a6c3fd4e8bc678
yg <yg@test.com>
2018-12-09 10:15:43 -0800

    2018-12-08

--- common/task_scripts/trackplot.py
+++ common/task_scripts/trackplot.py
@@ -18,11 +18,13 @@ all_track_targets = os.listdir('track_targets')
 
 titles = []
 dfs = []
+_range = []
 
 for track_target in all_track_targets:
     title = track_target.split('.')[0]
     df = pd.read_csv(
         'track_targets/{}'.format(track_target),
+        skiprows=1,
         header=None,
         index_col=0
     )
@@ -31,14 +33,33 @@ for track_target in all_track_targets:
     titles.append(title)
     dfs.append(df)
 
+    with open('track_targets/{}'.format(track_target)) as f:
+        first_line = f.readline()
+    low, high = first_line.split(',')
+    _range.append((float(low) - 0.1, float(high) + 0.1))
+
 if len(titles) == 1:
-    calmap.calendarplot(dfs[0].iloc[:,0],fillcolor='lightblue', vmin=0, fig_kws=dict(figsize=(24, 3)))
+    calmap.calendarplot(
+        dfs[0].iloc[:,0],
+        vmin = _range[0][0],
+        vmax=_range[0][1],
+        how=u'mean',
+        fillcolor='lightblue',
+        fig_kws=dict(figsize=(24, 3))
+    )
     plt.title(titles[0], fontsize=40)
 else:
     fig, ax = plt.subplots(nrows= len(titles), ncols=1, figsize = (24, 4 * len(titles)))
 
     for i in range(len(titles)):
-        calmap.yearplot(dfs[i].iloc[:,0],fillcolor='lightblue', vmin=0, ax=ax[i])
+        calmap.yearplot(
+            dfs[i].iloc[:,0],
+            how=u'mean',
+            fillcolor='lightblue',
+            vmin=_range[i][0],
+            vmax=_range[i][1],
+            ax=ax[i]
+        )
         ax[i].set_title(titles[i], fontsize=40)
 
 plt.show()
